c0333ef28043de986eb3fdcb4a93a454afdb1fd1
Kinjal Kathrecha <kinjalkathrecha2@gmail.com>
2026-02-12 16:36:57 +0530

    change-email-view

---
+++ core/serializers.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+
+class ChangeEmailSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    confirm_email = serializers.EmailField()
--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import FileUploadView
+from .views import FileUploadView,ChangeEmailView,UserEmailView
 app_name='core'
 
 urlpatterns=[
     path('demo/',FileUploadView.as_view(),name="file-upload-demo"),
+    path('email/',UserEmailView.as_view(),name="email"),
+    path('change-email/',ChangeEmailView.as_view(),name="change-email")
 ]
