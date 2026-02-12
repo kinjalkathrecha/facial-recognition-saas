43b356b4bc6f230415994c8b1cd76e5b9215353d
Kinjal Kathrecha <kinjalkathrecha2@gmail.com>
2026-02-12 13:29:47 +0530

    private routes

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,41 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom"; // Cleaned up imports
 import Hoc from "./hoc/hoc";
-
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import LandingPage from "./containers/LandingPage";
 import demo from "./containers/demo";
+
+// Added 'return' before the ternary operator
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const authenticated = localStorage.getItem("token") !== null;
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{
+            pathname: "/login",
+            state: { from: props.location }
+          }} />
+        )
+      }
+    />
+  );
+};
+
 const BaseRouter = () => (
   <Hoc>
-    <Route path="/login" component={Login} />
-    <Route path="/signup" component={Signup} />
-    <Route path="/demo" component={demo} />
-    <Route exact path="/" component={LandingPage} />
+    {/* It is highly recommended to wrap Routes in a <Switch> to prevent multiple matches */}
+    <Switch>
+      <Route exact path="/" component={LandingPage} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/demo" component={demo} />
+      <PrivateRoute path="/account" component={LandingPage} />
+    </Switch>
   </Hoc>
 );
 
